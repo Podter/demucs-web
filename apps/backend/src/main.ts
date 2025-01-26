e7efd9c6ab768e7a210694243048bcde6d5981b5
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2025-01-26 15:38:31 +0700

    Update main.ts

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -11,12 +11,20 @@ app.use(api);
 
 const STATIC_PATH = path.join(import.meta.dirname, "static");
 if (await fs.exists(STATIC_PATH)) {
-  app.use(
-    staticPlugin({
-      assets: STATIC_PATH,
-      prefix: "/",
-    }),
-  );
+  app
+    .onError((ctx) => {
+      if (ctx.code === "NOT_FOUND") {
+        ctx.set.status = "OK";
+        return Bun.file(path.join(STATIC_PATH, "index.html"));
+      }
+    })
+    .use(
+      staticPlugin({
+        assets: STATIC_PATH,
+        prefix: "/",
+        alwaysStatic: true,
+      }),
+    );
 }
 
 app.listen(3000, (server) => {
