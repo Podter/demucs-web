6983d37b05a9f6a060dc5d83dae73ee9adafef92
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2025-01-26 23:38:40 +0700

    added file reader

--- apps/backend/src/routes/file.ts
+++ apps/backend/src/routes/file.ts
@@ -12,7 +12,7 @@ export const file = new Elysia({ prefix: "/file" })
   })
   .get(
     "/:id/:filename",
-    ({ params, query, error }) => {
+    async ({ params, query, error, set, headers }) => {
       const hashType = getHashType(query.hash);
       const hash =
         hashType === "server"
@@ -25,11 +25,33 @@ export const file = new Elysia({ prefix: "/file" })
         return error(403);
       }
 
-      return s3.file(`${params.id}/${params.filename}`);
+      const file = s3.file(`${params.id}/${params.filename}`);
+
+      set.headers["Content-Type"] = file.type;
+      set.headers["Content-Disposition"] =
+        `inline; filename="${params.filename}"`;
+
+      const range = headers.Range;
+      if (!range) {
+        set.headers["Content-Length"] = file.size.toString();
+        return await file.arrayBuffer();
+      }
+
+      const start = Number(range.replace(/\D/g, ""));
+      // transfer 512KB at a time
+      const end = Math.min(start + 524288, file.size - 1);
+
+      set.headers["Content-Range"] = `bytes ${start}-${end}/${file.size}`;
+      set.headers["Content-Length"] = `${end - start + 1}`;
+
+      return await file.slice(start, end).arrayBuffer();
     },
     {
       query: t.Object({
         hash: t.String(),
+      }),
+      headers: t.Object({
+        Range: t.Optional(t.String()),
       }),
     },
   )
