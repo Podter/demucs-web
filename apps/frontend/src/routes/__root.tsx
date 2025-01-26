0951a7d0ac7f61a503e25fdaee3dc10a6707e33b
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2025-01-26 15:20:16 +0700

    devtools update

--- apps/frontend/src/routes/__root.tsx
+++ apps/frontend/src/routes/__root.tsx
@@ -1,5 +1,6 @@
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+
+import { TanStackRouterDevtools } from "~/components/router-devtools";
 
 export const Route = createRootRoute({
   component: () => (
