07be453b7eee0137bd7fb6b4ce6728ab549cd5cc
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2025-01-26 17:26:27 +0700

    added s3 env stuff

--- apps/backend/src/env.ts
+++ apps/backend/src/env.ts
@@ -6,6 +6,12 @@ export const env = createEnv({
     NODE_ENV: z.enum(["development", "production"]).default("development"),
     PORT: z.string().transform(Number).default("3000"),
     STATIC_PATH: z.string().default("static"),
+
+    S3_ACCESS_KEY_ID: z.string(),
+    S3_SECRET_ACCESS_KEY: z.string(),
+    S3_REGION: z.string(),
+    S3_ENDPOINT: z.string(),
+    S3_BUCKET: z.string(),
   },
   runtimeEnv: Bun.env,
   emptyStringAsUndefined: true,
