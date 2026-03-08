a8a3a01ba34a0f2ca9321ddf03276b615088d5dc
Podter <48936015+Podter@users.noreply.github.com>
2026-03-09 01:15:25 +0700

    added eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,92 @@
+// @ts-check
+
+import * as path from "node:path";
+import { includeIgnoreFile } from "@eslint/compat";
+import eslint from "@eslint/js";
+import astroParser from "astro-eslint-parser";
+import astroPlugin from "eslint-plugin-astro";
+import importPlugin from "eslint-plugin-import";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import reactPlugin from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+const baseConfig = defineConfig(
+  includeIgnoreFile(path.join(import.meta.dirname, ".gitignore")),
+  { ignores: ["**/*.config.*"] },
+  {
+    files: ["**/*.js", "**/*.ts", "**/*.tsx"],
+    plugins: {
+      import: importPlugin,
+    },
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+    ],
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/consistent-type-imports": [
+        "warn",
+        { prefer: "type-imports", fixStyle: "separate-type-imports" },
+      ],
+      "@typescript-eslint/no-misused-promises": [
+        2,
+        { checksVoidReturn: { attributes: false } },
+      ],
+      "@typescript-eslint/no-unnecessary-condition": [
+        "error",
+        {
+          allowConstantLoopConditions: true,
+        },
+      ],
+      "@typescript-eslint/no-non-null-assertion": "error",
+      "import/consistent-type-specifier-style": ["error", "prefer-top-level"],
+    },
+  },
+  {
+    linterOptions: { reportUnusedDisableDirectives: true },
+    languageOptions: {
+      parserOptions: {
+        project: true,
+      },
+    },
+  },
+);
+
+const reactConfig = defineConfig(
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    ...reactPlugin.configs.flat.recommended,
+    ...reactPlugin.configs.flat["jsx-runtime"],
+    ...jsxA11y.flatConfigs.recommended,
+    languageOptions: {
+      ...reactPlugin.configs.flat.recommended?.languageOptions,
+      ...reactPlugin.configs.flat["jsx-runtime"]?.languageOptions,
+      ...jsxA11y.flatConfigs.recommended.languageOptions,
+      globals: {
+        React: "writable",
+      },
+    },
+  },
+  reactHooks.configs.flat["recommended-latest"],
+);
+
+const astroConfig = defineConfig(...astroPlugin.configs.recommended, {
+  files: ["**/*.astro"],
+  languageOptions: {
+    parser: astroParser,
+    parserOptions: {
+      parser: tseslint.parser,
+      project: true,
+      extraFileExtensions: [".astro"],
+    },
+  },
+});
+
+export default defineConfig(baseConfig, reactConfig, astroConfig);
